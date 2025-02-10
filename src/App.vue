dfd9e0fa080f7c6be966ba176706e0385aee1735
stellamarma <stellamarmat@gmail.com>
2025-02-10 21:46:21 +0200

    Test

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+
+<template>
+    <el-radio-group v-model="size" aria-label="size control">
+      <el-radio-button value="large">large</el-radio-button>
+      
+    </el-radio-group>
+    <div class="demo-date-picker">
+      <div class="block">
+        <span class="demonstration">Default</span>
+        <el-date-picker
+          v-model="value1"
+          type="daterange"
+          range-separator="To"
+          start-placeholder="Start date"
+          end-placeholder="End date"
+          format="DD/MM/YYYY"
+          :size="size"
+        />
+      </div>
+      
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  import { ref } from 'vue'
+  
+  const size = ref<'default' | 'large' | 'small'>('default')
+  
+  const value1 = ref('')
+ 
+  
+
+  </script>
+  
+  <style scoped>
+  .demo-date-picker {
+    display: flex;
+    width: 100%;
+    padding: 0;
+    flex-wrap: wrap;
+  }
+  
+  .demo-date-picker .block {
+    padding: 30px 0;
+    text-align: center;
+    border-right: solid 1px var(--el-border-color);
+    flex: 1;
+  }
+  
+  .demo-date-picker .block:last-child {
+    border-right: none;
+  }
+  
+  .demo-date-picker .demonstration {
+    display: block;
+    color: var(--el-text-color-secondary);
+    font-size: 14px;
+    margin-bottom: 20px;
+  }
+  </style>
+  
+
